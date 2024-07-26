e382092ff9bec382df5beba0dfe3987ce83d86ec
yosinda <113226507+yosinda@users.noreply.github.com>
2024-07-26 20:36:39 +0900

    Improves stability of mouse
    - strobe samples every 25.6us and change timer to free-run
    - state resets if strobe=H is held over 200us
    - correct 1st data (upper nibble of delta-x)

--- rtl/ps2mouse.v
+++ rtl/ps2mouse.v
@@ -1,7 +1,8 @@
-
+// Outputs input signals from a PS/2 mouse in the MSX mouse protocol
+// Besed on ps2mouse module in MiSTer MSX core
 module ps2mouse
 (
-	input            clk,
+	input            clk,	// 20MHz
 	input            reset,
 
 	input            strobe,
@@ -14,68 +15,75 @@ wire [11:0] mdx = {{4{ps2_mouse[4]}},ps2_mouse[15:8]};
 wire [11:0] mdy = {{4{ps2_mouse[5]}},ps2_mouse[23:16]};
 
 always @(posedge clk) begin
-	reg  [5:0] count;
-	reg        old_clk;
-	reg        old_strobe;
+	reg  [6:0] old_strobe;
 	reg  [2:0] state = 0;
-	reg [14:0] timer = 0; //MSX mouse timer
+	reg  [8:0] timer = 0;	// 20MHz / 512 = 25.6us
 	reg  [7:0] mx;
 	reg  [7:0] my;
-	reg  [2:0] button;
 	reg [11:0] dx;
 	reg [11:0] dy;
 	reg        old_stb;
 
-	if(~&timer) timer <= timer + 1'b1;
-	old_strobe <= strobe;
-	
 	old_stb <= ps2_mouse[24];
 
 	if(reset) begin
-		dx     <= 0;
-		dy     <= 0;
-		data   <= 'b110000;
-		state  <= 0;
+		dx     	<= 0;
+		dy     	<= 0;
+		data   	<= 'b110000;
+		state  	<= 0;
+		old_strobe	<= 7'b0000000;
+		timer	<= 0;
 	end
 	else begin
+		timer <= timer + 1'b1;
 		if(old_stb ^ ps2_mouse[24]) begin
 			data[5:4] <= ~ps2_mouse[1:0];
 			dx <= dx - mdx;
 			dy <= dy + mdy;
 		end
 
-		case(state)
-			0: if(~old_strobe && strobe && &timer) begin
-					state <= state + 1'd1;
-					mx    <= dx[8:1];
-					my    <= dy[8:1];
-					dx    <= 0;
-					dy    <= 0;
-					timer <= 0;
+		// sampling strobe every 25.6us
+		if (&timer) begin
+			old_strobe[6:1] <= old_strobe[5:0];
+			old_strobe[0] 	<= strobe;
 
-					data[3:0] <= dx[7:4];
-				end
+			if ({old_strobe,strobe} == 8'h0) begin
+				// reset: hold L over 200us 
+				state <= 0;
+			end else begin
+				case(state)
+				0: if(~old_strobe[1] && old_strobe[0] && strobe) begin
+					// first strobe: hold H over 50us
+						state <= state + 1'd1;
+						mx    <= dx[8:1];
+						my    <= dy[8:1];
+						dx    <= 0;
+						dy    <= 0;
+						data[3:0] <= dx[8:5];
+					end
 
-			1: if(old_strobe && ~strobe) begin
-					state <= state + 1'd1;
-					data[3:0] <= mx[3:0];
-				end
+				1: if(old_strobe[0] && ~strobe) begin
+						state <= state + 1'd1;
+						data[3:0] <= mx[3:0];
+					end
 
-			2: if(~old_strobe && strobe) begin
-					state <= state + 1'd1;
-					data[3:0] <= my[7:4];
-				end
+				2: if(~old_strobe[0] && strobe) begin
+						state <= state + 1'd1;
+						data[3:0] <= my[7:4];
+					end
 
-			3: if(old_strobe && ~strobe) begin
-					state <= state + 1'd1;
-					data[3:0] <= my[3:0];
-				end
+				3: if(old_strobe[0] && ~strobe) begin
+						state <= state + 1'd1;
+						data[3:0] <= my[3:0];
+					end
 
-			4: if(~old_strobe && strobe) begin
-					state <= 0;
-					data[3:0] <= 0;
-				end
-		endcase
+				4: if(~old_strobe[0] && strobe) begin
+						state <= 0;
+						data[3:0] <= 0;
+					end
+				endcase
+			end
+		end
 	end
 end
 
